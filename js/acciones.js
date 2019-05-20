a8fd74c46cc0430cd1b42c908c4f21532eeaa703
Alejandroescobaramador <48726257+Alejandroescobaramador@users.noreply.github.com>
2019-05-20 11:49:51 -0500

    1

---
+++ js/acciones.js
@@ -0,0 +1,32 @@
+//JavaScript Document 
+
+ $(document).ready(function(e){
+	 document.addEventListener("deviceready",onDeviceReady,false);
+	 
+ });
+	 
+	 function onDeviceReady(){
+		 
+		 $('posicion').on('click',function(){
+		 getPosition();
+	 });
+	 }
+	 
+	 function getPosition(){
+		 var options ={
+			 enableHighAccuracy  :true,
+			 maximumAge:3600000
+		 }
+		 
+		 var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError,options);
+		 
+		  function onSuccess(Position){
+			  
+		     alert('Latitude:'   +Position.coords.latitude         +'/n'+
+			  'Longitude:'       +Position.coords.longitude        +'/n');
+		  };
+		  
+		   function onError(error){
+			   alert('code:'  +error.code  +'/n'+ 'message:'+ '/n');
+		   }
+	 }
